1e84719cb9df75ab6f4666b96071ba8378fb4f03
Tejaa Chintaluri <saitejaac@gmail.com>
2022-10-10 13:51:26 +0530

    Add getURLsFromHTML function with tests

--- crawl.js
+++ crawl.js
@@ -7,6 +7,27 @@ function normalizeURL(url){
   return fullPath
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+  const {JSDOM} = require('jsdom')
+  const dom = new JSDOM(htmlBody)
+  const aTagArray = dom.window.document.querySelectorAll('a')
+  const urlArray = []
+  for (const eachATag of aTagArray){
+    if (eachATag === null || eachATag === undefined){
+      continue
+    }
+    if (eachATag.href.slice(0,1) === '/') {
+      urlArray.push(`${baseURL}`+`${eachATag.href}`)
+      continue
+    }
+    urlArray.push(`${eachATag.href}`)
+  }
+  return urlArray
+}
+
+
+
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normalizeURL protocol', () => {
@@ -26,5 +26,35 @@ test('normalizeURL http', () => {
   const input = 'http://BLOG.boot.dev/path'
   const actual = normalizeURL(input)
   const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML relativeURLs', () => {
+  const base_url = "https://blog.boot.dev"
+  let html_string = `<html>
+    <body>
+        <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+        <a href="/hello"><span>Hello Page</span>/></a>
+    </body>
+</html>`
+  const urlarray = getURLsFromHTML(html_string, base_url)
+  const actual = urlarray[1]
+  const expected = "https://blog.boot.dev/hello"
+  expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML aTagsNum', () => {
+  const base_url = "https://blog.boot.dev"
+  const html_string = `<html>
+    <body>
+        <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+        <a href="/hello"><span>Hello Page</span>/></a>
+    </body>
+</html>`
+  const urlarray = getURLsFromHTML(html_string, base_url)
+  const actual = urlarray.length
+  const expected = 2
   expect(actual).toEqual(expected)
 })
