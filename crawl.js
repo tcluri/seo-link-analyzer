60a0c3e411424f0aca11b335434e1a94848e1cae
Tejaa Chintaluri <saitejaac@gmail.com>
2022-10-11 10:13:27 +0530

    Use recursion for crawlPage function

--- crawl.js
+++ crawl.js
@@ -26,8 +26,51 @@ function getURLsFromHTML(htmlBody, baseURL) {
 }
 
 
+async function crawlPage(baseURL, currentURL, pages={}) {
+   console.log(`Crawling ${baseURL}`)
+  // Check if the baseURL and currentURL are on the same domain
+  const baseDomain = new URL(baseURL)
+  const currentURLDomain = new URL(currentURL)
+  // console.log(`Base domain: ${baseDomain.hostname} \nCurrent domain: ${currentURLDomain.hostname}`)
+  if (baseDomain.hostname !== currentURLDomain.hostname){
+    return pages
+  }
+  // Normalized version of the currentURL
+  const normCurrentURL = normalizeURL(currentURL)
+  if (pages[normCurrentURL] > 0){
+    pages[normCurrentURL]++
+    return pages
+  }
+  // If no normCurrentURL is present, initialize with a 1
+  pages[normCurrentURL] = 1
+
+  try {
+    const fetchWebpage = await fetch(currentURL)
+    if (!fetchWebpage.ok){
+      console.log(`Error occcured: ${fetchWebpage.status}`)
+      return pages
+    }
+    const contentType = await fetchWebpage.headers.get('content-type')
+    if(!contentType.includes("text/html")){
+      console.log("Content type is not of type text/html")
+      return pages
+    }
+    const urlArray = getURLsFromHTML(await fetchWebpage.text(), baseURL)
+    // For each url in the webpage -- call the function recursively
+    for (eachURL of urlArray){
+      pages = await crawlPage(baseURL, eachURL, pages)
+    }
+  }
+  catch (err){
+    console.log(err.message)
+  }
+  return pages
+}
+
+
 
 module.exports = {
   normalizeURL,
-  getURLsFromHTML
+  getURLsFromHTML,
+  crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+const { crawlPage } = require('./crawl.js')
+
+async function main() {
+  const argvlength = process.argv.length
+  if (argvlength < 3){
+    console.log("No website given")
+  }
+  else if (argvlength > 3){
+    console.log("Cannot give more arguments than just the website")
+  }
+  else {
+    const baseURL = process.argv[2]
+    console.log(`Starting crawl of ${baseURL}`)
+    // Call crawlPage on baseURL to print the
+    // HTML output as a string
+    const pages = await crawlPage(baseURL, baseURL, {})
+    console.log(pages)
+  }
+}
+
+main()
